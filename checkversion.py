f4fcf347816bc8d8ae312ea5ffa4cd53277789e3
joshdeng <j.deng7@Hotmail.com>
2017-01-10 09:06:30 -0700

    add url

--- checkversion.py
+++ checkversion.py
@@ -1,9 +1,11 @@
+#!/usr/bin/env python
+
 import requests
 
 print requests .__version__
 
 
-response = requests.get("http://google.com/")
+response = requests.get("https://raw.githubusercontent.com/joshdeng/comput404lab1/master/checkversion.py")
 
 print response.status_code
 
